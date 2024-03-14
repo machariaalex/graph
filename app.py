14a01a5f77b09c6e363f2d3cf3321a18ee7a5a07
Alex Macharia <87443384+machariaalex@users.noreply.github.com>
2024-03-14 15:44:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -204,8 +204,8 @@ def main():
 
     # Visualization options
     st.sidebar.title("Visualization Options")
-    show_start_geofence_out_of_route = st.sidebar.checkbox("Show Start Geofence Out of Route")
-    show_end_geofence_out_of_route = st.sidebar.checkbox("Show End Geofence Out of Route")
+    show_start_geofence_out_of_route = st.sidebar.checkbox("Show Start Out of Geofence")
+    show_end_geofence_out_of_route = st.sidebar.checkbox("Show End Out of Geofence")
     show_within_geofence = st.sidebar.checkbox("Show Within the Geofence")
     show_out_of_route_network_diagram = st.sidebar.checkbox("Show Out of Geofence")
     show_fuel_consumption_comparison = st.sidebar.checkbox("Show Out of Geofence Fuel Consumption vs Within Geofence Fuel Consumption")
